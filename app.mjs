148eaabbaa55d7e7a98690ecefae34138dcad919
Nika K <firstnamenika@gmail.com>
2024-04-05 18:33:53 -0400

    populated repositories

--- app.mjs
+++ app.mjs
@@ -1,5 +1,33 @@
-import { fetchUser } from "./helper.mjs";
+import { fetchUser } from './getter.mjs';
 
-// import "helper2.mjs";
+const kweeuhreeObj = fetchUser('kweeuhree');
+// console.log(kweeuhreeObj);
+const firstnamenikaObj = fetchUser('firstnamenika');
+function showRepos(title) {
+    // console.log(title, ' inside show repos');
+    //toggle class of an element with id of title
+    const thisElement = document.getElementById(`hidden-${title}`);
+    console.log(thisElement, ' thisElelment inside showRepos'); 
+    // const thisClass = thisElement.classList[0];
+    let computedStyle = window.getComputedStyle(thisElement);
 
-fetchUser();
+    if(computedStyle.display === 'none') {
+        thisElement.style.display = 'flex';
+    } else {
+        thisElement.style.display = 'none';
+    }
+
+}
+function addEventListenerTitle() {
+    const titleList = document.getElementsByClassName('title');
+    for (let title of titleList) {
+        title.addEventListener('click', function(event) {
+            if (event.target.classList.contains('title')) {
+                // console.log(event.target.innerText, ' inside addEventListenerTitle');
+                showRepos(event.target.innerText);
+            }
+        });
+    }
+}
+
+addEventListenerTitle();
--- getter.mjs
+++ getter.mjs
@@ -1,19 +1,77 @@
+const getRepos = async (reposUrl) => {
+    try {
+        const response = await fetch(reposUrl);
+        if (response.ok) {
+            const urlArray = await response.json();
+            // console.log(urlArray);
+            for(let obj of urlArray) {
+                // console.log(obj);
+                addRepoItem(obj);  
+            }
+        }
+    } catch (error) {
+        console.log('error message inside getRepos ', error);
+    }
+}
 
+const addRepoItem = (obj) => {
+    const thisSection = document.getElementById(`section-${obj.owner.login}`);
+    // console.log(obj.owner.login, ' login');
+    const template = document.getElementById(`template-${obj.owner.login}`);
+    const repo = template.content.cloneNode(true);
+    
+    repo.querySelector('li a').innerHTML = obj.name;
 
+    repo.querySelector('li a').setAttribute('href', obj.html_url);
+    // test if theres time------------------------------------------
+    //const htmlUrl = obj.deployments_url;
+    //onclick open an overlay with the website and some basic info about it
+    //repo.querySelector('li a').addEventListener('click', function () => {
+        //showOverlay(this.htmlUrl);})
+    //---------------------------------------------------------------
+
+    // console.log(obj.name);
+
+    thisSection.appendChild(repo);
+}
+
+const displayUserInfo = (userName, numOfPublicRepos, blog, updatedAt) => {
+    // const thisSection = document.querySelector
+    console.log(numOfPublicRepos, 'inside display userInfo');
+}
+
+const daysSinceRegister = (createdAt) => {
+    console.log(createdAt);
+}
 //fetch user
-export const fetchUser = async () => {
+const fetchUser = async (userName) => {
     const baseUrl = `https://api.github.com/`;
-    const userName = 'kweeuhree';
+    // const userName = 'kweeuhree';
     const wordQuery = 'users/';
     const urlToFetch = `${baseUrl}${wordQuery}${userName}`;
     try {
         const response = await fetch(urlToFetch);
         if (response.ok) {
             const jsonResponse =  await response.json();
-            console.log(jsonResponse);
-            
+            // console.log(jsonResponse);
+            const reposUrl = jsonResponse.repos_url;
+            // console.log(reposUrl, ' reposUrl');
+
+            const numOfPublicRepos = jsonResponse.public_repos;
+            const blog = jsonResponse.blog;
+
+            const createdAt = jsonResponse.created_at;
+            const updatedAt = jsonResponse.updated_at;
+
+            getRepos(reposUrl);
+            displayUserInfo(userName, numOfPublicRepos, blog, updatedAt);
+            daysSinceRegister(createdAt);
         }
     } catch (error) {
         console.log(error);
     }
 }
+
+
+
+export { fetchUser };
